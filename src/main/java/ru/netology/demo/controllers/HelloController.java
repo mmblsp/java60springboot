e9cae4b25a3700805c3d957ea8ab03a8539ce6eb
Anatoly Zhuchkov <a.zhuchkov@arenadata.io>
2024-07-23 14:37:59 +0300

    start

---
+++ src/main/java/ru/netology/demo/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package ru.netology.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${hello.from:undefined}")
+    private String from;
+
+    @GetMapping("/")
+    private String hello() {
+        return String.format("Hello from %s", from);
+    }
+
+}
